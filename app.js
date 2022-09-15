6be6ada78e536b8b3ed317d184c09cac2c5fb7e9
luntik0011 <luntik0011@ya.ru>
2022-09-15 21:45:29 +0300

    Добавлен бот

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+//Инициализация покетов//////////////////////
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
+
+
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.command('oldschool', (ctx) => ctx.reply('Hello'));
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+
+//////////////////////////////////////////////////////////////
 const http = require('http');
 
 const hostname = '192.168.128.170';
